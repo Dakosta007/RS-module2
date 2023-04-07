32b376f18c0e6c79e38e3cc30581648463fe0b29
Dakosta007 <128571691+Dakosta007@users.noreply.github.com>
2023-04-08 01:56:46 +0500

    Update MyComponent.test.js

--- src/__tests__/MyComponent.test.js
+++ src/__tests__/MyComponent.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import MyComponent from '../MyComponent';
+import MyComponent from '../MyComponent.test.js';
 configure({ adapter: new Adapter() });
 describe('MyComponent', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<MyComponent />);
+    const wrapper = shallow(<MyComponent.test.js />);
     expect(wrapper).toMatchSnapshot();
   });
 });
