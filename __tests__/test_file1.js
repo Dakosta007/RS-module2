bbaa3ea4bd7520422270ee39dbbe51d4d1fdc32e
Dakosta007 <128571691+Dakosta007@users.noreply.github.com>
2023-04-08 01:21:25 +0500

    Delete __tests__ directory

--- __tests__/test_file1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import MyComponent from '../MyComponent';
-
-test('renders my component without crashing', () => {
-  const { getByText } = render(<MyComponent />);
-  const linkElement = getByText(/My Component/i);
-  expect(linkElement).toBeInTheDocument();
-});
