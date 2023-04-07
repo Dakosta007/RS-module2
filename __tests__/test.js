ff088fd86e515d39aac8d3ad72b3dff791065019
Dakosta007 <128571691+Dakosta007@users.noreply.github.com>
2023-04-07 10:08:06 +0500

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import MyComponent from '../MyComponent';
 
 describe('MyComponent', () => {
